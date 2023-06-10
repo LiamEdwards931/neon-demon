fef7fcaa82aa605d6ffff338fecfd92d573facc4
LiamEdwards931 <>
2023-06-10 11:20:23 +0000

    neon demon home page with style

---
+++ assets/css/Javascript/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("#back-to-top-button").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("back-to-top-button").style.marginLeft= "0";
+  }
